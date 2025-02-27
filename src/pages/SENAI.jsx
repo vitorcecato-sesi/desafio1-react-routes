7bcb3e546ef8ddd5dc755f91f42e10795d24e62a
Extreme <npyetro26@gmail.com>
2025-02-27 14:27:23 -0300

    NavaBar

---
+++ src/pages/SENAI.jsx
@@ -0,0 +1,18 @@
+import Header from "../components/Header";
+
+function Senai(props){
+    return(
+        <>
+    <Header/>
+    <img className='img' src={props.imagem1}/>
+<article>
+    <h1>Sobre SENAI</h1>
+    <p></p>
+</article>
+    </>
+    )
+}
+
+
+
+export default Senai;
