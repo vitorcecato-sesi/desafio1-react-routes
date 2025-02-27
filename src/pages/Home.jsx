06629cdaee41b7a8363290e0f6d1e4943c55ce15
Vitor Cecato <vitor.cecatosesi@gmail.com>
2025-02-27 13:34:54 -0300

    Estilo

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+function Home(){
+    return(
+        <>
+            {/* <Navbar/> */}
+            <section>
+                <h1>🌳 Bem-vindo à HomePage! 🏡</h1>
+                <p>Está é a página inicial do nosso site.</p>
+            </section>
+        </>
+    )
+}
+
+export default Home
