d2607d65e6c72d3b3ad418ea896e44bfc2429218
Vitor Cecato <vitor.cecatosesi@gmail.com>
2025-02-27 15:52:30 -0300

    Corrigir homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,9 +6,17 @@
     import Header from "../components/Header"
     import Footer from "../components/Footer"
     import Valores from "../components/aside"
+    import Integrantes from "../components/Integrantes"
 //.
 
 // Fotos
+    import fotoPerfilLauraB from "../assets/fotoUsuarioLauraB.png"
+    import fotoPerfilLucas from "../assets/fotoUsuarioLucas.png"
+    import fotoPerfilMilena from "../assets/fotoUsuarioMilena.png"
+    import fotoPerfiloPietro from "../assets/fotoUsuarioPietroM.png"
+    import fotoPerfilPyetro from "../assets/fotoUsuarioPyetro.png"
+    import fotoPerfilVitor from "../assets/fotoUsuarioVitorG.png"
+
     // Footer
     import foto from "../assets/gmail.png"
     import foto2 from "../assets/instagram.png"
@@ -27,7 +35,44 @@ function Home(){
                         <article className="conteudo">
                             <h1>🌳 Boas vindas a HomePage 🏡</h1>
                             <p>Está é a nossa Homepage que foi criada como parte de um desafio!</p>
-                            <img src="https://img.freepik.com/vetores-gratis/ilustracao-encantadora-de-uma-casa-de-desenho-animado_1308-176076.jpg" alt="Casa" />
+                            <section>
+                                <section className="blocoImg">
+                                <Integrantes 
+                                    foto= {fotoPerfilLauraB} 
+                                    nome="Laura Betti" 
+                                    hobbies="Os meus hobbies são: Ouvir música e ver filmes/séries."
+                                />
+                                <Integrantes 
+                                    foto= {fotoPerfilLucas} 
+                                    nome="Lucas Casagrande" 
+                                    hobbies="Os meus hobbies são: Praticar vôlei, jogar jogos online e assistir séries."
+                                />
+                                </section>
+                                <section className="blocoImg">
+                                <Integrantes 
+                                    foto= {fotoPerfilMilena} 
+                                    nome="Milena Souza" 
+                                    hobbies="Os meus hobbies são: Escutar música, ver filmes e séries, estudar matemática e cozinhar."
+                                />
+                                <Integrantes 
+                                    foto= {fotoPerfiloPietro} 
+                                    nome="Pietro Melle" 
+                                    hobbies="Os meus hobbies são: Praticar esportes."
+                                />
+                                </section>
+                                <section className="blocoImg">
+                                <Integrantes 
+                                    foto= {fotoPerfilPyetro} 
+                                    nome="Pyetro Joaquim" 
+                                    hobbies="Os meus hobbies são: Jogar jogos online, ouvir música e cozinhar."
+                                />
+                                <Integrantes 
+                                    foto= {fotoPerfilVitor} 
+                                    nome="Vitor Cecato" 
+                                    hobbies="Meus hobbies são: jogar jogos online, programar e nadar."
+                                />
+                                </section>
+                            </section>
                         </article>
                         <Valores
                             valor1="União"
