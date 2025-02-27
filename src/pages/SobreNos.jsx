7be7b79bab484fedfb03b51a70b6769e8a611016
Lucas C <lucascasagrandesesi@gmail.com>
2025-02-27 14:36:54 -0300

    Mesclando

--- src/pages/SobreNos.jsx
+++ src/pages/SobreNos.jsx
@@ -37,20 +37,16 @@ return (
             foto={fotoPerfilLauraB}
             // Aqui estamos passando o nome como propriedade para o componente "Perfil"
             nome="@Laura_betti"
-            // Aqui estamos passando os hobbies como propriedade para o componente "Perfil"
-            hobbies="Sou a Laura Betti, tenho 17 anos e moro em Louveira , gosto de ver series/filmes. 🎬"
           />
 
           <UsuarioCard
             foto={fotoPerfilLucas} 
             nome="@Lucas_casagrande" 
-            hobbies="Sou o Lucas Casagrande, tenho 17 anos e moro em Vinhedo e adoro jogar volei e jogos online com meus amigos. 🏐" 
           />
 
           <UsuarioCard 
             foto={fotoPerfilMilena} 
             nome="@Milena_souza" 
-            hobbies="Sou a Milena, tenho 16 anos e gosto de passar o tempo com meus amigos e famílias. 👨‍👩‍👧" 
           />
         </section>
 
@@ -58,19 +54,16 @@ return (
           <UsuarioCard 
             foto={fotoPietro} 
             nome="@Pietro_Melle" 
-            hobbies="Sou o Pietro Melle, tenho 16 anos e moro em Jundiaí. Adoro jogar com os meus amigos e andar de bicicleta. 🚴‍♀️" 
           />
 
           <UsuarioCard
             foto={fotoPyetro} 
             nome="@Pyetro_Joaquim" 
-            hobbies="Sou o Pyetro Joaquim, tenho 17 anos e moro em Valinhos. Gosto de cozinhar e jogar. 🎮" 
           />
 
           <UsuarioCard
             foto={fotoVitor} 
             nome="@Vitor_Geraldo" 
-            hobbies="Sou o Vitor Geraldo, tenho 17 anos e moro em Valinhos. Amo nadar e programar. 🏊‍♂️" 
           />
         </section>
       </main>
